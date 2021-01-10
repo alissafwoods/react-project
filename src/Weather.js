45e9e72fc847ce695712ccef05a8d851b6760913
afw <71993863+alissafwoods@users.noreply.github.com>
2021-01-09 16:50:07 -0800

    Began coding Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,49 @@
 import React from "react";
-import Weather from "./Weather.css";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+  let weatherData = {
+    city: "Victoria",
+    updated: "Thursday 19:58",
+    temperature: 11,
+    conditions: "Sunny",
+    humidity: 60,
+    wind: 3,
+  };
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col-4">
+          <div className="card" styles="width: 150px">
+            <div className="card-body">
+              <p className="today-temperature">
+                <span>{weatherData.temperature}°</span>
+                <br />
+                <a href="/">C</a> |<a href="/"> F</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-8">
+          <h1>{weatherData.city}</h1>
+          <h3>
+            Last updated: <span>{weatherData.updated}</span>
+          </h3>
+          <ul>
+            <li>{weatherData.conditions}</li>
+            <li>
+              Humidity: <span>{weatherData.humidity}</span>%
+            </li>
+            <li>
+              Wind Speed: <span>{weatherData.wind}</span> km/hr
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <div className="row"></div>
+      <br />
+      <script src="src/script.js"></script>
+    </div>
+  );
 }
