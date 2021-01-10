6cd42368df72ca115b194b12e6de98412cc69ade
afw <71993863+alissafwoods@users.noreply.github.com>
2021-01-09 16:20:24 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a
-          href="https://github.com/alissafwoods/react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open source code
-        </a>
-        by Alissa Woods
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/alissafwoods/react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+          by Alissa Woods
+        </footer>
+      </div>
     </div>
   );
 }
